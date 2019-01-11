522875f80488e7f342643162ae89ce0c1864eaf4
Phill Barron <Phill.Barron@bbc.co.uk>
2019-01-11 08:53:25 +0000

    added file-system write

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 const secretService = require('@bbc/cps-mi6')
+const fs = require('fs');
 
-const writeSecretToFileSystem = async(path, secret) => {
+const writeSecretToFileSystem = async(path, secretsId, secret) => {
     try {
-        const secretObject = await secretService.getSecrets(secret);
-        console.log('secrectObject', secretObject);
+        const secretObject = await secretService.getSecrets(secretsId);
+        const secretToWriteToFileSystem = secretObject[secret];
+        fs.writeFile(path, secretToWriteToFileSystem, (error) => {
+            if(error) {
+                return console.log(error);
+            }
+            console.log(`${secret} value was written to ${path} successfully`);
+        });
     } catch (error) {
         console.log(`failed to retrieve ${secret}`)
     }
 }
+ 
+
 
 module.exports = {
     writeSecretToFileSystem
