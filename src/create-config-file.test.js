1db934db5ce931d72e5f5a0d10b71264a239262e
Phill Barron <phillipbarron242@gmail.com>
2019-01-13 19:22:23 +0000

    added failing test

---
+++ src/create-config-file.test.js
@@ -0,0 +1,13 @@
+jest.mock("fs", () => ({
+    writeFile: jest.fn()
+}));
+const fs = require('fs');
+const secretService = require('@bbc/cps-mi6')
+const secretWriter = require('./index');
+
+test.skip('writes whole secret to fileSystem', () =>{
+    secretService.getSecrets = jest.fn(()=> Promise.resolve({foo: "bar"}));
+    secretWriter.writeSecretToFileSystem('foo', 'bar');
+    
+    expect(fs.writeFile).toBeCalledWith(1);
+});
