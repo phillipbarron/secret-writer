3a08e46acd4b7381c7d60a4375a2b555a58b437d
Phill Barron <phillipbarron242@gmail.com>
2019-07-14 19:27:25 +0100

    checking output

--- src/create-config-file.test.js
+++ src/create-config-file.test.js
@@ -2,11 +2,40 @@ jest.mock("fs", () => ({
   writeFile: jest.fn(),
   readFileSync: jest.fn()
 }));
+
 const fs = require("fs");
 const secretService = require("@bbc/cps-mi6");
 const secretWriter = require("./index");
 
+beforeEach(() => {
+  jest.resetAllMocks();
+});
+
 test("writes whole secret to fileSystem", async () => {
+  secretService.getSecrets = jest.fn(() => Promise.resolve({ foo: "bar" }));
+  await secretWriter.writeSecretToFileSystem("foo", "bar");
+
+  expect(fs.writeFile).toBeCalledTimes(1);
+});
+
+test("logs an error where exception thrown calling secret service", async () => {
+  secretService.getSecrets = jest.fn(() => Promise.reject("foo"));
+  global.console = {
+    log: jest.fn()
+  };
+
+  await secretWriter.writeSecretToFileSystem("foo", "bar");
+
+  expect(fs.writeFile).toBeCalledTimes(0);
+  expect(global.console.log).toBeCalledWith("failed to retrieve bar", "foo");
+});
+
+test("logs error where exception thrown writing to fs", async () => {
+  jest.mock("fs", () => ({
+    writeFile: jest.fn(() => {}),
+    readFileSync: jest.fn()
+  }));
+
   secretService.getSecrets = jest.fn(() => Promise.resolve({ foo: "bar" }));
   await secretWriter.writeSecretToFileSystem("foo", "bar");
 
--- src/index.js
+++ src/index.js
@@ -1,23 +1,27 @@
-const secretService = require('@bbc/cps-mi6')
-const fs = require('fs');
+const secretService = require("@bbc/cps-mi6");
+const fs = require("fs");
 
-const writeSecretToFileSystem = async(path, secretsId, secret) => {
-    try {
-        const secretObject = await secretService.getSecrets(secretsId);
-        const secretToWriteToFileSystem = secret ? secretObject[secret] : JSON.stringify(secretObject, null, 2);
-        if(secretToWriteToFileSystem) {
-            fs.writeFile(path, secretToWriteToFileSystem, (error) => {
-                if(error) {
-                    return console.log(error);
-                }
-                console.log(`${secret||secretsId} value was written to ${path} successfully`);
-            });
+const writeSecretToFileSystem = async (path, secretsId, secret) => {
+  try {
+    const secretObject = await secretService.getSecrets(secretsId);
+    const secretToWriteToFileSystem = secret
+      ? secretObject[secret]
+      : JSON.stringify(secretObject, null, 2);
+    if (secretToWriteToFileSystem) {
+      fs.writeFile(path, secretToWriteToFileSystem, error => {
+        if (error) {
+          return console.log(error);
         }
-    } catch (error) {
-        console.log(`failed to retrieve ${secret}`, error)
+        console.log(
+          `${secret || secretsId} value was written to ${path} successfully`
+        );
+      });
     }
-}
+  } catch (error) {
+    console.log(`failed to retrieve ${secretsId}`, error);
+  }
+};
 
 module.exports = {
-    writeSecretToFileSystem
-}
+  writeSecretToFileSystem
+};
