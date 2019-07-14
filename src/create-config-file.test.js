963f4fa26facafa50f203d1258c408d9aef6896f
Phill Barron <phillipbarron242@gmail.com>
2019-07-14 17:52:08 +0100

    security update

--- src/create-config-file.test.js
+++ src/create-config-file.test.js
@@ -1,13 +1,14 @@
 jest.mock("fs", () => ({
-    writeFile: jest.fn()
+  writeFile: jest.fn(),
+  readFileSync: jest.fn()
 }));
-const fs = require('fs');
-const secretService = require('@bbc/cps-mi6')
-const secretWriter = require('./index');
+const fs = require("fs");
+const secretService = require("@bbc/cps-mi6");
+const secretWriter = require("./index");
 
-test.skip('writes whole secret to fileSystem', () =>{
-    secretService.getSecrets = jest.fn(()=> Promise.resolve({foo: "bar"}));
-    secretWriter.writeSecretToFileSystem('foo', 'bar');
-    
-    expect(fs.writeFile).toBeCalledWith(1);
+test.skip("writes whole secret to fileSystem", () => {
+  secretService.getSecrets = jest.fn(() => Promise.resolve({ foo: "bar" }));
+  secretWriter.writeSecretToFileSystem("foo", "bar");
+
+  expect(fs.writeFile).toBeCalledWith(1);
 });
